4cd4da7ae15789ad5a971180e50fb899b0b2e896
kyurko <iurko.kk@phystech.edu>
2026-04-07 20:03:31 +0300

    add: create stucture

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include "Modules/data/DataModule.h"
+#include "Modules/prepare/PrepareModule.h"
+#include "Modules/core/CoreModule.h"
+#include "Modules/output/OutputModule.h"
+
+int main() {
+    DataModule data;
+    PrepareModule prep;
+    CoreModule core;
+    OutputModule output;
+
+    return 0;
+}
